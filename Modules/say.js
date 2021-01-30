5f1cccc95ce5801f25236cef99228ea1719ae692
funydop <54532893+funydop@users.noreply.github.com>
2021-01-31 00:35:34 +0100

    Update say.js

--- Modules/say.js
+++ Modules/say.js
@@ -1,5 +1,5 @@
 const  Discord = require("discord.js");
-module.exports.run = async (client, message,config,args) => {
+module.exports.run = async (message,args,config) => {
 if(args.lenght > 1 && args[0].indexOf('<') && args[0].indexOf('>') && args[0].indexOf('#'))
 {
   message.channel.send("salut");
