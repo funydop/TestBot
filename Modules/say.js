d553003ace4793391a2e96ad0891d91ebec314ed
funydop <54532893+funydop@users.noreply.github.com>
2021-02-01 21:33:21 +0100

    Modification de commande Say

--- Modules/say.js
+++ Modules/say.js
@@ -1,13 +1,34 @@
-const  Discord = require("discord.js");
-module.exports.run = async (message,args,config) => {
-if(args.lenght > 1 && args[0].indexOf('<') && args[0].indexOf('>') && args[0].indexOf('#'))
-{
-  message.channel.send("salut");
+const Discord = require("discord.js");
+module.exports.run = async (message, args, config) => {
 
-}
-else
-{
-  throw Error ("Mauvais syntaxe");
-}
+  if (args.length > 1 && args[0].indexOf('<') != '-1' && args[0].indexOf('>') != '-1' && args[0].indexOf('#') != '-1') {
+    let titre = message.content.split(args[0])[1];
+    let filter = m => m.author.id === message.author.id
+    message.channel.send("veullez inserez votre texte").then( msg =>{
+      setTimeout(() => {
+        msg.delete()
+      }, 1000);
+    });
+
+    const collector = message.channel.createMessageCollector(filter, { max: 1 });
+    collector.on('collect', messageCollect => {
+
+      
+      let messageSend = new Discord.MessageEmbed()
+        .setFooter(message.guild.name, message.guild.iconURL())
+        .setAuthor(message.author.username, message.author.avatarURL())
+        .setTimestamp()
+        .setColor(7339827)
+        .addField(titre, messageCollect.content);
+      message.channel.send(messageSend);
+      message.delete();
+      m.delete();
+
+    })
+
+  }
+  else {
+    throw Error("Mauvaise Syntaxe : `"+ config.Prefix+"say [titre]`");
+  }
 
 }
