3fa030e506bcea0f0d0bf224c6ee7b5683c72545
funydop <54532893+funydop@users.noreply.github.com>
2021-02-01 22:14:56 +0100

    Maj Say + Modification Config Service

--- Modules/say.js
+++ Modules/say.js
@@ -1,9 +1,9 @@
 const Discord = require("discord.js");
-module.exports.run = async (message, args, config) => {
+module.exports.run = async (client,message, args, config) => {
 
   if (args.length > 1 && args[0].indexOf('<') != '-1' && args[0].indexOf('>') != '-1' && args[0].indexOf('#') != '-1') {
     let titre = message.content.split(args[0])[1];
-    let filter = m => m.author.id === message.author.id
+    let filter = m => m.author.id === message.author.id;
     message.channel.send("veullez inserez votre texte").then( msg =>{
       setTimeout(() => {
         msg.delete()
@@ -12,23 +12,33 @@ module.exports.run = async (message, args, config) => {
 
     const collector = message.channel.createMessageCollector(filter, { max: 1 });
     collector.on('collect', messageCollect => {
-
-      
-      let messageSend = new Discord.MessageEmbed()
+      salon = message.guild.channels.cache.get(args[0].replace('<#','').replace('>',''));
+      console.log(salon.type);
+      if(!isNaN(salon) && salon.type == "text")
+      {
+        let messageSend = new Discord.MessageEmbed()
         .setFooter(message.guild.name, message.guild.iconURL())
         .setAuthor(message.author.username, message.author.avatarURL())
         .setTimestamp()
         .setColor(7339827)
         .addField(titre, messageCollect.content);
-      message.channel.send(messageSend);
+      salon.send(messageSend);
       message.delete();
-      m.delete();
+      messageCollect.delete();
 
+
+
+
+      }
+      else
+      {
+        throw Error("Merci de mettre un salon Textuel et Valide");
+      }
     })
 
   }
   else {
-    throw Error("Mauvaise Syntaxe : `"+ config.Prefix+"say [titre]`");
+    throw Error("Mauvaise Syntaxe : `"+ config.Prefix+"say [salon] [titre]`");
   }
 
 }
