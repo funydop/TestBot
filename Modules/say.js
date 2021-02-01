ddf37a0dcf723f22c2568aae75a4a3c77369b758
funydop <54532893+funydop@users.noreply.github.com>
2021-02-01 19:16:35 +0100

    Update say.js

--- Modules/say.js
+++ Modules/say.js
@@ -9,8 +9,5 @@ else
 {
   throw Error ("Mauvais syntaxe");
 }
-console.log("message : \n" + message)
-console.log("client : \n" + client)
-console.log("Config :  \n" + config)
-console.log("args : \n" + args)
+
 }
