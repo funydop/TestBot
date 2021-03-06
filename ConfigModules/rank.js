689ca1a97fb00df99891fc49b42894d1b72edb8c
funydop <54532893+funydop@users.noreply.github.com>
2021-03-06 03:36:29 +0100

    Add files via upload

---
+++ ConfigModules/rank.js
@@ -0,0 +1,32 @@
+const Discord = require("discord.js");
+const fs = require("fs");
+module.exports.run = async (action, pathFile) => {
+
+    let result = (action =="edit") ? editModule() : CreateModule(pathFile);
+    return result
+}
+function CreateModule(pathFile){
+    const callback = function (err) {
+        if (err)
+            console.log("Creation du fichier KO Erreur : " + err);
+        else
+            console.log("Creation du fichier du serveur OK ");
+    };
+    // Création de l'object De base 
+    objectGlobal = new Object();
+    objectGlobal.RolesWhitList = [""];
+    objectGlobal.Hierarchie =[{"role":"","ordre":1}];
+    objectGlobal.SalonRankUp = "";
+    objectGlobal.OtherRoleWelcom = [""]
+    // Conversion Object To Json
+    objectGlobal = JSON.stringify(objectGlobal);
+    console.log(objectGlobal);
+    console.log(pathFile);
+    fs.writeFile(pathFile, objectGlobal, callback)
+}
+
+function editModule()
+{
+
+    //TOODO
+}
