83f2f5aaea82ad236b9ec0d6e2d37f8cdb98d14f
funydop <54532893+funydop@users.noreply.github.com>
2021-03-06 12:09:48 +0100

    Add files via upload

--- Modules/sugg.js
+++ Modules/sugg.js
@@ -2,25 +2,23 @@ const  Discord = require("discord.js");
 
 
 module.exports.run = async (client, message, args, config) => {
-if(!config.salonSuggestion)
+if(!config.salonCommande)
 throw Error("Paramètre : salonSuggestion manquant dans la configuration sugg.json");
-if(message.channel.id != config.salonSuggestion)
-throw Error ("Merci de vous rendre dans le salon <#"+config.salonSuggestion+">");
+if(message.channel.id != config.salonCommande)
+throw Error ("Merci de vous rendre dans le salon <#"+config.salonCommande+">");
 
 var messagePlayer = message;
  var content = messagePlayer.content.split(' ')[1];
- console.log(content);
 if(!content)  
 throw Error("merci de mettre un message valide");
-console.log(message.guild.iconURL());
 const suggestion = new Discord.MessageEmbed()
         .setFooter(message.guild.name,message.guild.iconURL())
         .setTitle("__**✭ New Suggestion de "+message.author.username+" ✭**__")
         .setTimestamp()
         .setColor(7339827)
         .setDescription(messagePlayer.content.replace("!sugg "));
-
-messagePlayer.channel.send(suggestion).then(msg=>{
+let salon = message.guild.channels.cache.get("809537090030141533");
+salon.send(suggestion).then(msg=>{
     msg.react('✔');
     msg.react('❌');
 })
