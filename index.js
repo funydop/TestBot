e854ea893864f616e5612833f52dc1971a22fe11
funydop <54532893+funydop@users.noreply.github.com>
2021-04-04 16:33:28 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const discord = require("discord.js");
 const fs = require("fs");
 const allIntents = discord.Intents.ALL;
 const client = new discord.Client({ ws: { intents: allIntents } });
-client.login("Njc1NzcwNDIzNzExMTA1MDI1.Xj7-QA.vVk1TQ93YIfYYP8Ootq3GRsGlKU");
+client.login("ODA0ODc0NDgzMTY5MDk5Nzg2.YBSrxg.DLfimjK3lqtW2IxoomJ1QGE1718");
 client.commands = new discord.Collection();
 client.configCommands = new discord.Collection();
 // const { Client, Intents } = require('discord.js');
@@ -14,30 +14,30 @@ client.configCommands = new discord.Collection();
 const pathDirectoryDataServer = './ServerData/';
 
 const loadCommands = (dir = "./Modules") => {
-        const commands = fs.readdirSync(`${dir}/`).filter(files => files.endsWith(".js"));
-        
-        for (const file of commands) {
-            const getFileName = require(`${dir}/${file}`);
-            client.commands.set(getFileName.help.name, getFileName);
-            console.log(`✅ commande : ${getFileName.help.name}`);
-        };
+    const commands = fs.readdirSync(`${dir}/`).filter(files => files.endsWith(".js"));
+
+    for (const file of commands) {
+        const getFileName = require(`${dir}/${file}`);
+        client.commands.set(getFileName.help.name, getFileName);
+        console.log(`✅ commande : ${getFileName.help.name}`);
+    };
 };
 
 const loadEvents = (dir = "./Events/") => {
-        const events = fs.readdirSync(`${dir}/`).filter(files => files.endsWith(".js"));
+    const events = fs.readdirSync(`${dir}/`).filter(files => files.endsWith(".js"));
 
-        for (const event of events) {
-            const evt = require(`${dir}/${event}`);
-            const evtName = event.split(".")[0];
-            client.on(evtName, evt.bind(null, client));
-            console.log(`✅ événement : ${evtName}`);
-        };
+    for (const event of events) {
+        const evt = require(`${dir}/${event}`);
+        const evtName = event.split(".")[0];
+        client.on(evtName, evt.bind(null, client));
+        console.log(`✅ événement : ${evtName}`);
+    };
 
 };
 
-const loadConfigCommands = (dir = "./ConfigModules") =>{
+const loadConfigCommands = (dir = "./ConfigModules") => {
     const configCommands = fs.readdirSync(`${dir}/`).filter(files => files.endsWith(".js"));
-        
+
     for (const file of configCommands) {
         const getFileName = require(`${dir}/${file}`);
         client.configCommands.set(getFileName.help.name, getFileName);
@@ -54,13 +54,31 @@ console.log("===Load ConfigCommande===")
 loadConfigCommands();
 
 
-client.on('guildMemberAdd', member =>{
-    client.user.setActivity("Membre : "+member.guild.memberCount,{ type: 'WATCHING' });
+client.on('guildMemberAdd', member => {
+    client.user.setActivity("Membre : " + member.guild.memberCount, { type: 'WATCHING' });
 })
-client.on('guildMemberRemove', member =>{
-    client.user.setActivity("Membre : "+member.guild.memberCount,{ type: 'WATCHING' });
+client.on('guildMemberRemove', member => {
+    client.user.setActivity("Membre : " + member.guild.memberCount, { type: 'WATCHING' });
 })
 
+
+client.CheckPermission = (client, message) => {
+    let authorRole = message.guild.member(message.author)._roles;
+    let roleFind = false
+
+    authorRole.forEach(element => {
+        console.log();
+        if (client.dataModule.RolesWhitList.find(x => x == element))
+            roleFind = true;
+    })
+
+    if (message.guild.member(message.author).hasPermission("ADMINISTRATOR"))
+        roleFind = true;
+
+    console.log(roleFind);
+    return roleFind;
+
+}
 
 String.isNullOrEmpty = function (value) {
     return !(typeof value === "string" && value.length > 0);
